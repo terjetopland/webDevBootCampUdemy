0af962d516de85cff72cb22d89cf4ff714ee6e3e
terjetop <terjetop@uia.no>
2024-03-15 09:25:47 +0100

    Added the functionality to view the modal when button is clicked.

--- 08_Bootstrap/Modal/modal.js
+++ 08_Bootstrap/Modal/modal.js
@@ -1,5 +1,6 @@
 console.log(`Hello modal!`);
 
+
 $(document).ready(function () {
     $('#carousel-login-register').on('slid.bs.carousel', function () {
         var slideIndex = $(this).find('.carousel-item.active').index();
@@ -14,30 +15,48 @@ $(document).ready(function () {
 
 let numberDay = new Date().getDay();
 let day = ``;
-
+const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
 switch (numberDay) {
     case 2:
-        day = `tuesday. A bit better!`;
+        day = `${days[2]}. A bit better!`;
         break;
     case 3:
-        day = `wednesday. Little saturday!`;
+        day = `${days[3]}. Little-${days[6]}!`;
         break;
     case 4:
-        day = `thursday. Soon friday!`;
+        day = `${days[4]}. Soon ${days[5]}!`;
         break;
     case 5:
-        day = `friday. Finally weekend!`;
+        day = `${days[5]}. Finally weekend!`;
         break;
     case 6:
-        day = `saturday. Let's chill!`;
+        day = `${days[6]}. Let's chill!`;
         break;
     case 0:
-        day = `sunday. Go on relax!`;
+        day = `${days[0]}. Go on relax!`;
         break;
     default:
-        day = `monday. Uggh!`;
-
+        day = `${days[1]}. Uggh!`;
 }
 
 document.getElementById('day-of-week').innerHTML = day;
 
+// let userDataFromPrompt = '';
+// if(numberDay > 4) {
+//     userDataFromPrompt = prompt('It is weekend!!! Give me a word!');
+//     document.getElementById('user-input-word').innerHTML = `Your word was: ${userDataFromPrompt.toUpperCase()}`;
+// } 
+
+const myModal = document.getElementById('my-modal');
+
+const logInRegisterBtn = document.getElementById('log-in-register-btn');
+
+const closeMyModalBtn = document.getElementById('my-modal-close-btn')
+
+logInRegisterBtn.onclick = () => {
+    myModal.style.visibility = 'visible';
+}
+
+closeMyModalBtn.onclick = () => {
+    myModal.style.visibility = 'hidden';
+}
